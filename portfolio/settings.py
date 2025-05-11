58e9790a6c2eb8d7b484f2cd3fff408a11b9e3a5
Manali Choudhary <manalich696@gmail.com>
2025-05-11 14:03:36 +0530

    Updated ALLOWED_HOSTS for new Render app URL

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#+z#*lz!i2s%(a5mkdt2+fdwy_o*j&nu7g6&cc%e$intu32wz@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['portfolio-6-0ki3.onrender.com']
+ALLOWED_HOSTS = ['mytechcareer.onrender.com']
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
